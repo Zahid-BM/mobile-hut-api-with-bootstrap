86ac25db442c1bdd9cc864944529d5c68f09072a
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-03-01 21:27:49 +0600

    check no -1 is okay

--- js/test.js
+++
@@ -1,9 +0,0 @@
-<p>
-    <b> <u>Specifications :</u></b> <br> <br>
-        <u>Chip set</u> - ${displayDetails.mainFeatures.chipSet}<br>
-            <u>Display </u>- ${displayDetails.mainFeatures.displaySize}<br>
-                <u>Memory</u> - ${displayDetails.mainFeatures.memory}<br>
-                    <u>Sensors</u> - ${displayDetails.mainFeatures.sensors}<br>
-                        <u>Others</u> - Bluetooth: ${displayDetails.others?.Bluetooth ?? "No"}, GPS: ${displayDetails.others?.GPS ?? "No"}, NFC: ${displayDetails.others?.NFC ?? "No"} , Radio: ${displayDetails.others?.Radio ?? "No"}, USB: ${displayDetails.others?.USB ?? "No"}, WLAN: ${displayDetails.others?.WLAN ?? "No"}
-
-                    </p>
