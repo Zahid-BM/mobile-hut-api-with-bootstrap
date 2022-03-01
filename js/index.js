54f1f1ce7d9f29a99ab4a85117830aa6b4ebdaf3
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-03-01 08:54:17 +0600

    last known good commit

--- js/index.js
+++ js/index.js
@@ -9,15 +9,16 @@ const searchPhone = () => {
 const displayPhone = phones => {
     const display = document.getElementById('display');
     phones.forEach(phone => {
+        // console.log(phone);
         const foundPhones = document.createElement('div');
         foundPhones.innerHTML = `
          <div class="col h-100">
             <div class="card h-100">
                 <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title text-white fw-bolder">${phone.brand}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${phone.phone_name}</h6>
-                     <button onclick = "phoneDetails()" class="btn btn-warning text-white fw-bold" > Details</button>
+                    <h5 class="card-title">${phone.brand}</h5>
+                     <h6 class="card-subtitle mb-2 text-muted">${phone.phone_name}</h6>
+                     <button onclick = "phoneDetails('${phone.slug}')" class="btn btn-warning text-white fw-bold" > Details</button>
                 </div>
             </div>
   </div> 
@@ -25,3 +26,32 @@ const displayPhone = phones => {
         display.appendChild(foundPhones);
     });
 }
+const phoneDetails = phoneIdentity => {
+    console.log(phoneIdentity);
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneIdentity}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetails(data.data));
+}
+const displayPhoneDetails = displayDetails => {
+    // console.log(displayDetails.mainFeatures.chipSet);
+    const phoneDetails = document.getElementById('details');
+    const foundDetails = document.createElement('div');
+    foundDetails.innerHTML = `
+    <div class="col h-100">
+            <div class="card h-100">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.brand}</h5>
+                     <h6 class="card-subtitle mb-2 text-muted">${phone.phone_name}</h6>
+                     <button onclick = "phoneDetails('${phone.slug}')" class="btn btn-warning text-white fw-bold" > Details</button>
+                </div>
+            </div>
+  </div> 
+    
+    
+    
+    `
+}
+
+
