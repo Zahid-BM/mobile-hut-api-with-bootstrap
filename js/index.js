fa57346d92c35807fe96eeff9a30330112387b83
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-03-01 18:30:23 +0600

    last known good commit

--- js/index.js
+++ js/index.js
@@ -1,17 +1,48 @@
 /* onclick function to fetch data */
 const searchPhone = () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=${input.value}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhone(data.data));
+    if (input.value.length == '') {
+        alert('Wrong or empty input !!! Please, input correctly and try again.')
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${input.value}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhone(data.data.splice(0, 20))); /* set phpne qtty will be shown up to 20 */
+        /*  clear input field  */
+        input.value = '';
+        /* clear previous searched phones that were in desplay */
+        const display = document.getElementById('display');
+        display.innerHTML = ''; /* this is preferable */
+        /* clear previous phone details on UI */
+        const phoneDetails = document.getElementById('details');
+        phoneDetails.innerHTML = '';
+    }
 }
 /* function for displaying found data on UI  */
 const displayPhone = phones => {
+    console.log(phones);
     const display = document.getElementById('display');
-    phones.forEach(phone => {
-        // console.log(phone);
-        const foundPhones = document.createElement('div');
-        foundPhones.innerHTML = `
+    if (phones.length == 0) {
+        const div = document.createElement('div');
+        div.classList.add('mx-auto', 'w-100', 'h-50');
+        div.innerHTML = `
+   <div class="col">
+            <div class="card h-100">
+                <div class="card-body bg-danger">
+                    <h5 class="card-title text-center text-white fw-bolder"> Oppss ! Searched Phone is Unavailable !!!</h5>
+                </div>
+            </div>
+  </div>
+    
+    `;
+        display.appendChild(div);
+
+    }
+    else {
+        phones.forEach(phone => {
+            // console.log(phone);
+            const foundPhones = document.createElement('div');
+            foundPhones.innerHTML = `
          <div class="col h-100">
             <div class="card h-100">
                 <img src="${phone.image}" class="card-img-top" alt="...">
@@ -23,8 +54,11 @@ const displayPhone = phones => {
             </div>
   </div> 
         `
-        display.appendChild(foundPhones);
-    });
+            display.appendChild(foundPhones);
+        })
+    }
+
+
 }
 const phoneDetails = phoneIdentity => {
     console.log(phoneIdentity);
@@ -32,6 +66,9 @@ const phoneDetails = phoneIdentity => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhoneDetails(data.data));
+    /* clear previous data of phone details */
+    const phoneDetails = document.getElementById('details');
+    phoneDetails.innerHTML = '';
 }
 const displayPhoneDetails = displayDetails => {
     console.log(displayDetails);
@@ -43,6 +80,16 @@ const displayPhoneDetails = displayDetails => {
                 <img src="${displayDetails.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${displayDetails.brand} ${displayDetails.name} </h5>
+                        <p>${displayDetails.releaseDate}</p>
+                        <p>
+                        <b> <u>Specifications :</u></b> <br> <br>
+                  <u>Chip set</u> - ${displayDetails.mainFeatures.chipSet}<br> 
+                  <u>Display </u>- ${displayDetails.mainFeatures.displaySize}<br> 
+                  <u>Memory</u> - ${displayDetails.mainFeatures.memory}<br> 
+                  <u>Sensors</u> - ${displayDetails.mainFeatures.sensors}<br>
+                <u>Others</u> - Bluetooth: ${displayDetails.others?.Bluetooth}, GPS: ${displayDetails.others?.GPS}, NFC: ${displayDetails.others?.Bluetooth}, Radio: ${displayDetails.others?.Radio}, USB: ${displayDetails.others?.USB}, WLAN: ${displayDetails.others?.WLAN}
+ 
+                        </p>
                 </div>
             </div>
   </div> 
